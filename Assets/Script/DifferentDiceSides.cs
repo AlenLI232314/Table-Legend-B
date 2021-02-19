c9a36858cea936ac2be6038e7c6a0c732be20ab8
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2021-02-19 01:04:00 -0600

    adding interfaces and entity script

---
+++ Assets/Script/DifferentDiceSides.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Dice { Four, Six, Eight, Ten, Twelve, Twenty };
+public class DifferentDiceSides
+{
+    public int FourSidedDieResult;
+    public int SixSidedDieResult;
+    public int EightSidedDieResult;
+    public int TenSidedDieResult;
+    public int TwelveSidedDieResult;
+    public int TwentySidedDieResult;
+
+    public void RollingDice(Dice Die)
+    {
+        switch (Die)
+        {
+            case Dice.Four:
+                FourSidedDieResult = UnityEngine.Random.Range(1, 5);
+                break;
+
+            case Dice.Six:
+                SixSidedDieResult = UnityEngine.Random.Range(1, 7);
+                break;
+
+            case Dice.Eight:
+                EightSidedDieResult = UnityEngine.Random.Range(1, 9);
+                break;
+
+            case Dice.Ten:
+                TenSidedDieResult = UnityEngine.Random.Range(1, 11);
+                break;
+
+            case Dice.Twelve:
+                TwelveSidedDieResult = UnityEngine.Random.Range(1, 13);
+                break;
+
+            case Dice.Twenty:
+                TwentySidedDieResult = UnityEngine.Random.Range(1, 21);
+                break;
+        }
+    }    
+}
---
+++ Assets/Script/Entity.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour, IDamageable<int>, IKillable
+{
+    public int HP { get; set; }
+    public bool isAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void DealDamage(int DamageDealt, Entity entity, Dice die)
+    {
+        entity.HP -= DamageDealt;
+    }
+
+    public void Kill()
+    {
+        this.isAlive = false;
+    }
+
+    public void OnKill()
+    {
+        //does nothing yet
+    }
+
+    public void HealDamage(int DamageHealed)
+    {
+        this.HP += DamageHealed;
+    }
+
+    public void TakeDamage(int DamageTaken)
+    {
+        this.HP -= DamageTaken;
+    }
+
+    public void OnDamage()
+    {
+        //does nothing yet
+    }
+}
