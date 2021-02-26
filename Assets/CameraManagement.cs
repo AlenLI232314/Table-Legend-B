404ac9e100c9a8edaf9a942f23316d136dc5716a
JakeRe <jake.reilly.20@gmail.com>
2021-02-26 07:57:53 -0600

    Added CineMachine and Some Camera Work.

---
+++ Assets/CameraManagement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManagement : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private bool worldCamera = true;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeCameras()
+    {
+        if (worldCamera)
+        {
+            animator.Play("CombatCamera");
+        }
+        else if (!worldCamera)
+        {
+            animator.Play("WorldCamera");
+        }
+        worldCamera = !worldCamera;
+
+    }
+}
