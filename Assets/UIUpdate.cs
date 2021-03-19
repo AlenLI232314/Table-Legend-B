7fafbf714e35cabda85ebb4abc50daf52c42c646
JakeRe <jake.reilly.20@gmail.com>
2021-03-19 08:46:29 -0500

    Player Health UI Now Reflects Damage Taken In Combat

--- Assets/UIUpdate.cs
+++ Assets/UIUpdate.cs
@@ -7,19 +7,24 @@ using UnityEngine.UI;
 public class UIUpdate : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private int Gold;
-    [SerializeField] private TextMeshProUGUI goldText;
-    [SerializeField] private Character character;
 
+    [SerializeField] private int Gold;
+    [SerializeField] private int Health;
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private Character character;
     void Start()
     {
         character = FindObjectOfType<Character>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Health = character.HP;
         Gold = character.gold;
         goldText.text = Gold.ToString();
+        healthText.text = Health.ToString();
     }
 }
