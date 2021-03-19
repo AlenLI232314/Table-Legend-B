ae6ed3109e22958e69ef176001a26245e9a10808
JakeRe <jake.reilly.20@gmail.com>
2021-03-19 13:43:22 -0500

    Character now scales down when entering combat.

--- Assets/Script/Events/Monster.cs
+++ Assets/Script/Events/Monster.cs
@@ -11,15 +11,21 @@ public class Monster : MonoBehaviour
     public CinemachineVirtualCamera cam;
     public static event  System.Action <CinemachineVirtualCamera> cameraEvent;
     public GameObject boardUI;
-    public GameObject combatUICanvas;
+    public GameObject playerGO;
+
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip battleStart;
+    [SerializeField] private Vector3 originalScale;
 
     void Start()
     {
+
         fightWarnning.SetActive(false);
         cam.gameObject.SetActive(false);
         audioSource = GetComponent<AudioSource>();
+
+        playerGO = GameObject.FindGameObjectWithTag("Player");
+        originalScale = playerGO.transform.localScale;
     }
 
 
@@ -27,6 +33,7 @@ public class Monster : MonoBehaviour
     {
         if (player.gameObject.tag == "Player" )
         {
+            playerGO.transform.localScale = new Vector3 (.15f, .3f, .15f);
             audioSource.PlayOneShot(battleStart);
             cameraEvent?.Invoke(cam);
             fightWarnning.SetActive(true);
