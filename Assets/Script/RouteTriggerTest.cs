7440f3b517daba6b1d3129a09d5de996e921b5e0
AlenLi <shilian.li@loop.colum.edu>
2021-04-22 03:33:35 -0400

    Temple Space Fixed

--- Assets/Script/RouteTriggerTest.cs
+++ Assets/Script/RouteTriggerTest.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class RouteTriggerTest : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
-
-    //set trigger on when player enter the collider
 
     void OnTriggerEnter(Collider other)
     {
