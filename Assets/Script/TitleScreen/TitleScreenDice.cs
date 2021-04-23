940e6e5a103798311bdf8b181a7134d3817090b2
Ryan R <mrcheesehed1000@gmail.com>
2021-04-22 20:08:59 -0500

    added dice rotatation

--- Assets/Script/TitleScreen/TitleScreenDice.cs
+++ Assets/Script/TitleScreen/TitleScreenDice.cs
@@ -12,19 +12,32 @@ public class TitleScreenDice : MonoBehaviour
 
     //The position of the dice so that it's always in the same space on screen even if the cam moves
     Vector3 dicePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        //Sets the position (the camera's current pos with offsets added
+        FollowCam();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowCam();
+        DiceRotate();
+    }
+
+    //Sets the position (the camera's current pos with offsets added
+    void FollowCam()
+    {
         dicePos = new Vector3(mainCam.gameObject.transform.position.x + 0.614f,
                                mainCam.gameObject.transform.position.y - 1.425f,
                                 mainCam.gameObject.transform.position.z + 0.82f);
         this.gameObject.transform.position = dicePos;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Rotates the dice
+    void DiceRotate()
     {
-        this.gameObject.transform.position = dicePos;
+        this.gameObject.transform.Rotate(.0f, -.1f, -.0f);
     }
 }
