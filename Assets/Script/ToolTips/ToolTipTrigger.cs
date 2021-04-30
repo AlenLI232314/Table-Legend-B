9db50d709f7f7ffe5921f0a3c0260e96d7da1aef
AlenLi <shilian.li@loop.colum.edu>
2021-04-30 07:51:43 -0400

    Tried to add fade in animations to tool tips.

--- Assets/Script/ToolTips/ToolTipTrigger.cs
+++ Assets/Script/ToolTips/ToolTipTrigger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ToolTipTrigger : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 {
@@ -9,20 +10,65 @@ public class ToolTipTrigger : MonoBehaviour,IPointerEnterHandler,IPointerExitHan
     public string header;
     [Multiline()]
     public string content;
+    public CanvasGroup uiElement;
+
+
 
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        LeanTween.delayedCall(0.5f, () =>
+        //FadeIn();
+        //ToolTipSystem.Show(content, header);
+        LeanTween.delayedCall(1f, () =>
         {
             ToolTipSystem.Show(content, header);
-        });
 
+
+        });
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         ToolTipSystem.Hide();
 
+        LeanTween.cancel(delay.uniqueId);
+
     }
+
+    //public void FadeIn()
+    //{
+
+    //    StartCoroutine(FadeToolTips(uiElement, uiElement.alpha, 1));
+    //}
+
+    //public void FadeOut()
+    //{
+    //    StartCoroutine(FadeToolTips(uiElement, uiElement.alpha, 0));
+    //}
+
+    //public IEnumerator FadeToolTips(CanvasGroup cg, float start, float end, float lerpTime = 3f)
+    //{
+    //    float _timeStartedLearping = Time.time;
+    //    float timeSinceStarted = Time.time - _timeStartedLearping;
+    //    float percentageComplete = timeSinceStarted / lerpTime;
+
+    //    while (true)
+    //    {
+    //        timeSinceStarted = Time.time - _timeStartedLearping;
+    //        percentageComplete = timeSinceStarted / lerpTime;
+
+    //        float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+    //        cg.alpha = currentValue;
+
+    //        if (percentageComplete >= 1) break;
+
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //}
+
+
+
+
+
 }
